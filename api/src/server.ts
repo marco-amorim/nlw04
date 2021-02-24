8069a4130433dbf7135cfad5cf9360daabc12436
marco-amorim <marcoamorim95@hotmail.com>
2021-02-23 21:19:18 -0300

    first day

---
+++ api/src/server.ts
@@ -0,0 +1,21 @@
+import express, { response } from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+	return res
+		.json({
+			message: 'Hello world',
+		})
+		.status(200);
+});
+
+app.post('/', (req, res) => {
+	return res
+		.json({
+			message: 'User saved!',
+		})
+		.status(201);
+});
+
+app.listen(3333, () => console.log('Server running'));
