6c734fdc839d165a50c91326b40bb24408829d26
marco-amorim <marcoamorim95@hotmail.com>
2021-03-26 17:03:39 -0300

    controller function to get all surveys

--- api/src/controllers/SurveyController.ts
+++ api/src/controllers/SurveyController.ts
@@ -17,6 +17,14 @@ class SurveyController {
 
 		return response.status(201).json(survey);
 	}
+
+	async show(request: Request, response: Response) {
+		const surveysRepository = getCustomRepository(SurveysRepository);
+
+		const surveys = await surveysRepository.find();
+
+		return response.json(surveys);
+	}
 }
 
 export { SurveyController };
